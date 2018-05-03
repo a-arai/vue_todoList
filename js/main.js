013f31f6d62b0c2d1661083c6f8c250874fecfc2
a-arai <aya.arai.2a@gmail.com>
2018-05-03 16:49:47 +0900

    READMEの修正、localStorageを使った仕様に変更

--- js/main.js
+++ js/main.js
@@ -1,52 +1,47 @@
 (function() {
     'use strict';
 
-    //two way data binding 
-
-    var vm = new Vue({
+    let vm = new Vue({
         el: '#app',
         data: {
             newItem: '',
-            todos: [{
-                title: 'task 1',
-                isDone: true
-            },{
-                title: 'task 2',
-                isDone: false
-            },{
-                title: 'task 3',
-                isDone: false
-            }
-            
+            todos: [
                 
-
             ]
         },
+        watch: {
+            todos: {
+                handler() {
+                    localStorage.setItem('todos', JSON.stringify(this.todos));
+                },
+                deep: true
+            }  
+        },
+        mounted: function() {
+            this.todos = JSON.parse(localStorage.getItem('todos')) || [];
+        },
         methods: {
-            addItem: function() {
-                var item = {
+            addItem() {
+                let item = {
                     title: this.newItem,
                     isDone: false
-
-                }
+                };
                 this.todos.push(item);
                 this.newItem = '';
             },
-            deleteItem: function(index) {
-                if (confirm('are you sure?')) {
-                    this.todos.splice(index, 1);
-                }
+            deleteItem(index) {
+                this.todos.splice(index, 1);
+            },
+            allDelete() {
+                this.todos = this.remaining;
             }
         },
         computed: {
-            remaining: function() {
-                var items = this.todos.filter(function(todo) {
+            remaining() {
+                return this.todos.filter(function(todo) {
                     return !todo.isDone;
                 });
-                return items.length;
             }
         }
-        
     });
-
 })();
